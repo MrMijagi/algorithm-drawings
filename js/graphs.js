c9e3ff171b4669e183be609de388a42aabaae2f2
MrMijagi <dawid.bilinski.99@gmail.com>
2021-03-29 17:53:14 +0200

    added simple graphs

---
+++ js/graphs.js
@@ -0,0 +1,63 @@
+function node(x, y, radius, value) {
+  new paper.Path.Circle({
+    center: [x, y],
+    radius: radius,
+    strokeColor: '#000',
+    strokeWidth: 2,
+    fillColor: '#fff'
+  });
+
+  new paper.PointText({
+    point: [x, y + (radius * 0.25)],
+    content: value,
+    fillColor: '#000',
+    fontFamily: 'Courier New',
+    fontWeight: 'bold',
+    fontSize: radius * 0.8,
+    justification: 'center'
+  });
+}
+
+function connect_nodes(x1, y1, x2, y2, color) {
+  var path = new paper.Path({
+    segments: [[x1, y1], [x2, y2]]
+  });
+  path.style = {
+    strokeWidth: 2,
+    strokeColor: color
+  };
+}
+
+function get_node_offset(x, y, tile_size) {
+  var x_offset = (x * tile_size) + (tile_size / 2);
+  var y_offset = (y * tile_size) + (tile_size / 2);
+
+  return [x_offset, y_offset];
+}
+
+export function graph(nodes, connections, directed, x_pos, y_pos, tile_size, scale) {
+  Object.keys(connections).forEach(function(index) {
+    var val = connections[index];
+
+    var node_start = nodes[val[0]];
+    var node_end = nodes[val[1]];
+
+    var start_offset = get_node_offset(node_start[0], node_start[1], tile_size);
+    var end_offset = get_node_offset(node_end[0], node_end[1], tile_size);
+
+    connect_nodes(
+      x_pos + start_offset[0],
+      y_pos + start_offset[1],
+      x_pos + end_offset[0],
+      y_pos + end_offset[1],
+      val[2]
+    );
+  });
+
+  Object.keys(nodes).forEach(function(index) {
+    var val = nodes[index];
+    var offset = get_node_offset(val[0], val[1], tile_size);
+
+    node(x_pos + offset[0], y_pos + offset[1], tile_size * scale, val[2]);
+  });
+}
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { red_black_tree } from './js/red_black_tree.js';
 import { grid } from './js/labyrinth.js';
+import { graph } from './js/graphs.js';
 
 var tree = { 
   1:["104","red"], 
@@ -41,12 +42,30 @@ var map2 = [
   [' ', 'v', 'x', ''],
 ];
 
+var nodes = {
+  1:[0, 0, '1'],
+  2:[4, 0, '2'],
+  3:[4, 2, '3'],
+  4:[2, 2, '4'],
+  5:[2, 1, '5'],
+  6:[0, 2, '6'],
+}
+
+var connections = {
+  1:[1, 2, '#00f'],
+  2:[2, 3, '#0f0'],
+  3:[3, 4, '#f00'],
+  4:[4, 5, '#000'],
+  5:[5, 6, '#f0f']
+}
+
 window.onload = function() {
   var canvas = document.getElementById("myCanvas");
   paper.setup(canvas);
 
-  red_black_tree(tree, 800, 50, 20, 0.5, 1);
-  grid(map, 20, 20, 30, 30);
+  //red_black_tree(tree, 800, 50, 20, 0.5, 1);
+  //grid(map2, 20, 20, 30, 30);
+  graph(nodes, connections, false, 10, 10, 50, 0.4);
 
   paper.view.draw();
 }
